e8992ff2e0d5370a97b7e4d8543ff1f5cc8bde70
Nick Niemeir <nick.niemeir@gmail.com>
2013-01-21 16:38:22 -0700

    Download common dependencies

--- prefab.js
+++ prefab.js
@@ -3,6 +3,8 @@ var fs = require('fs')
   , exec = require('child_process').exec
   // Dependencies
   , es = require('event-stream')
+  , request = require('request')
+  // Other top level vars
   , opts = { name: process.argv[2] }
   , dir = path.join(process.cwd(), opts.name)
 
@@ -15,15 +17,34 @@ exec('git config -l', function (e, stuff) {
   opts.author = stuff.match(/user\.name=(.+)/)[1] + ' <' + stuff.match(/user\.email=(.+)/)[1] + '>'
   opts.year = 2013
 
+  var dep = path.join(dir, 'public', 'dep')
   var dirs =
     [ dir
     , path.join(dir, 't')
     , path.join(dir, 'bin')
     , path.join(dir, 'public')
+    , dep
     ]
 
   mkdirs(dirs, function (e) {
     if (e) throw e
+
+    // Download common dependencies
+    ;[ 'http://code.jquery.com/jquery.min.js'
+    , 'http://backbonejs.org/backbone-min.js'
+    , 'http://hay.github.com/stapes/stapes.min.js'
+    , 'http://underscorejs.org/underscore-min.js'
+    ].forEach(function (url) {
+      var req = request(url)
+      req.pipe(fs.createWriteStream(path.join(dep, url.split('/').pop())))
+      req.on('error', function (e) {
+        console.log('Could not download ' + url +' because of ' + e)
+      })
+      req.on('end', function () {
+        console.log('Downloaded ' + url)
+      })
+    })
+
 
     ;[ 'package.json'
     , 'readme.md'
@@ -32,6 +53,7 @@ exec('git config -l', function (e, stuff) {
     , '.gitignore'
     , 'bin/cli.js'
     ].forEach(function (file) {
+      console.log('Copying over ' + file)
       fs.createReadStream(path.join(__dirname, 'plate', file))
         .pipe(replacer(opts))
         .pipe(fs.createWriteStream(path.join(dir, file)))
