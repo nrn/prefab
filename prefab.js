e86ca10ffeeecbcedda5e9c8e7ef3ed8687ea2a0
Nick Niemeir <nick.niemeir@gmail.com>
2013-04-07 07:00:32 +0000

    New replacer

--- prefab.js
+++ prefab.js
@@ -3,6 +3,7 @@
 var fs = require('fs')
   , path = require('path')
   , exec = require('child_process').exec
+  , stream = require('stream')
   // Dependencies
   , es = require('event-stream')
   , request = require('request')
@@ -23,38 +24,38 @@ exec('git config -l', function (e, stuff) {
 
   var dep = path.join(dir, 'public', 'dep')
   var src =  path.join(__dirname, 'plate')
+  var template = Object.keys(opts).map(function (key) {
+     return new RegExp('#{' + key + '}#', 'g')
+  })
   glob(path.join(src, '**'), function (e, files) {
     files.forEach(function (file) {
       console.log('Copying over ' + file)
+      var replacer = new stream.Transform
+      replacer._transform = function (data, enc, cb) {
+        cb(null, template.reduce(function (item, regex) {
+          return new Buffer(item.toString().replace(item))
+        }, data))
+      }
       fstream.Reader(file)
-        .pipe(replacer(opts))
+        .pipe(replacer)
         .pipe(fstream.Writer(path.join(dir, path.relative(src, file))))
     })
-    //;[ 'http://code.jquery.com/jquery.min.js'
-    //, 'http://backbonejs.org/backbone-min.js'
-    //, 'http://hay.github.com/stapes/stapes.min.js'
-    //, 'http://underscorejs.org/underscore-min.js'
-    //].forEach(function (url) {
-    //  var req = request(url)
-    //  req.pipe(fs.createWriteStream(path.join(dep, url.split('/').pop())))
-    //  req.on('error', function (e) {
-    //    console.log('Could not download ' + url +' because of ' + e)
-    //  })
-    //  req.on('end', function () {
-    //    console.log('Downloaded ' + url)
-    //  })
-    //})
+    ;[ 'http://code.jquery.com/jquery.min.js'
+    , 'http://backbonejs.org/backbone-min.js'
+    , 'http://hay.github.com/stapes/stapes.min.js'
+    , 'http://underscorejs.org/underscore-min.js'
+    ].forEach(function (url) {
+      var req = request(url)
+      req.pipe(fstream.Writer(path.join(dep, url.split('/').pop())))
+      req.on('error', function (e) {
+        console.log('Could not download ' + url +' because of ' + e)
+      })
+      req.on('end', function () {
+        console.log('Downloaded ' + url)
+      })
+    })
 
   })
-
 
 })
-
-function replacer (opts) {
-  return es.map(function (data, cb) {
-    cb(null, Object.keys(opts).reduce(function (d, key) {
-      return d.replace(new RegExp('#{' + key + '}#', 'g'), opts[key])
-    }, data.toString()))
-  })
-}
 
